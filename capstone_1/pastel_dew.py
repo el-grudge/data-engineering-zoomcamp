54ba326c134297c11d6510a28f7fc569c310be80
el_grudge <minasonbol@gmail.com>
2024-03-19 04:29:44 +0000

    add capstone first pipeline

---
+++ capstone_1/pastel_dew.py
@@ -0,0 +1,68 @@
+import dlt 
+import requests
+import os
+
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+def paginated_getter(BASE_API_URL, blog_main_url, n_pages=1):
+    page_number = 0
+
+    while page_number <= n_pages:
+        # Set the query parameters
+        params = {'json':'true', 'page': page_number}
+
+        # Make the GET request to the API
+        response = requests.get(f'{BASE_API_URL}{blog_main_url}', params=params)
+        response.raise_for_status()  # Raise an HTTPError for bad responses
+        page_json = response.json()['result']['content']['modules']
+        print(f'got page number {page_number} with {len(page_json)} records')
+
+        # if the page has no records, stop iterating
+        if page_json:
+            yield page_json
+            page_number += 1
+        else:
+            # No more data, break the loop
+            break
+
+# Load environment variables from the .env file
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/src/personal-gcp.json'
+# make this parameterized to be set by user
+project_id = 'linen-source-411501'
+
+@data_loader
+def load_data(*args, **kwargs):
+    """
+    Template code for loading data from any source.
+
+    Returns:
+        Anything (e.g. data frame, dictionary, array, int, str, etc.)
+    """
+    BASE_API_URL = kwargs['BASE_API_URL']
+    blog_main_url = kwargs['blog_main_url']
+    # Define your pipeline
+    pipeline = dlt.pipeline(
+        pipeline_name='my_pipeline',
+        destination='bigquery',
+        dataset_name='money-diaries-test'
+        )
+
+    n_pages=0
+    pipeline.run(
+        paginated_getter(BASE_API_URL, blog_main_url, n_pages=n_pages), 
+        table_name="diary_links", 
+        write_disposition="replace"
+        )
+    
+    return {}
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
